9f4062f33e08d83073a9842c2814a0c50d2ac4c6
stonewell <jingnan.si@gmail.com>
2016-06-21 22:55:10 -0700

    add stub for values

--- src/state/macd.py
+++ src/state/macd.py
@@ -5,14 +5,18 @@
 '''
 
 def get_states_demension():
-    return (3, 3, 3)
+    return (3, 3, 3, 3, 3)
 
 def get_states(values):
     dif = values.get_dif()
     dea = values.get_dea()
+    dif_changes = values.get_dif_changes()
+    dea_changes = values.get_dea_changes()
     
     s_dif = 0 if dif > 0 else 1 if dif < 0 else 2
     s_dea = 0 if dea > 0 else 1 if dea < 0 else 2
     s_dif_dea = 0 if dif > dea else 1 if dif < dea else 2
+    s_dif_changes = 0 if dif_changes > 0 else 1 if dif_changes < 0 else 2
+    s_dea_changes = 0 if dea_changes > 0 else 1 if dea_changes < 0 else 2
     
-    return (s_dif, s_dea, s_dif_dea)
+    return (s_dif, s_dea, s_dif_dea, s_dif_changes, s_dea_changes)
---
+++ src/values.py
@@ -0,0 +1,18 @@
+class Values(object):
+    def __init__(self):
+        pass
+
+    def get_dif(self):
+        pass
+
+    def get_dea(self):
+        pass
+
+    def get_dif_changes(self):
+        pass
+
+    def get_dea_changes(self):
+        pass
+    
+    def get_changes(self):
+        pass
