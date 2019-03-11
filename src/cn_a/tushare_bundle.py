b7a89754a26020881af2617bc43a5bd2ee3aea87
stonewell <jingnan.si@gmail.com>
2019-03-10 22:55:30 -0700

    start add tushare bundle and calendars

---
+++ src/cn_a/tushare_bundle.py
@@ -0,0 +1,15 @@
+
+
+def ingest(environ,
+           asset_db_writer,
+           minute_bar_writer,
+           daily_bar_writer,
+           adjustment_writer,
+           calendar,
+           start_session,
+           end_session,
+           cache,
+           show_progress,
+           output_dir):
+    print(start_session, end_session)
+    pass
---
+++ src/extension.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+from zipline.data.bundles import register
+from cn_a.tushare_bundle import ingest as tushare_ingest
+from zipline.utils.calendars import register_calendar
+
+from cn_a.tushare_calendar import CNAExchangeCalendar
+
+
+start_session = pd.Timestamp('2016-1-1', tz='utc')
+end_session = pd.Timestamp('2018-1-2', tz='utc')
+
+register_calendar('CN_A',
+                  CNAExchangeCalendar(),
+                  True)
+
+register(
+    'cn_a_tushare_bundle',
+    tushare_ingest,
+    calendar_name='CN_A', # CN Shanghai
+    start_session=start_session,
+    end_session=end_session
+)
